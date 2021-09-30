0f8c88983f4ae655811d95aadeb247bca4649254
Jezreel Salinas <jsalinas@cpp.edu>
2021-09-29 19:47:23 -0700

    Added a new file for guessing game

---
+++ Numb_Guessing.c
@@ -0,0 +1,16 @@
+/*
+NUMBER GUESSING GAME
+A program that allows a user to guess a number between 1-10
+*/
+
+
+#include <stdio.h>
+
+void displayMenu(){
+    printf("Press 1 to play a game");
+    printf("Press 2 to change the max number");
+    printf("Press 3 to quit");
+}
+int main(){
+    return 0;
+}
