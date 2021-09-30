34896076e7a21ccba2ea8f98af73fe7a0caf7e6d
Jezreel Salinas <jsalinas@cpp.edu>
2021-09-29 21:54:18 -0700

    Added more lines on input validation. Fixed some of the bugs. Added a clear buffer function to clear everything in a buffer to fix the bug.

--- Numb_Guessing.c
+++ Numb_Guessing.c
@@ -11,19 +11,23 @@ A program that allows a user to guess a number between 1-10
 const int DEFAULT_VALUE = 10;
 const char STOP = 'S';  /// a character used to stop the game and go back to the menu
 const char CONTINUE = 'C'; /// a character used to continue the game if the input is invalid or the guess input is incorrect.
+const char TERMINATE = 'T'; /// Terminates a program once done playing the game
 
+void clear_bufferinput(){
+    while(getchar() != '\n');
+}
 ///Displays the menu
 void displayMenu(){
-    printf("Press 1 to play a game");
-    printf("Press 2 to change the max number");
-    printf("Press 3 to quit");
+    printf("Press 1 to play a game\n");
+    printf("Press 2 to change the max number\n");
+    printf("Press 3 to quit\n");
 }
 
 //Checks for input. Returns an appropriate input
 char inputValid(){
     char d;
     fflush(stdout);
-    d = getchar();
+    scanf("%c", &d);
     while(!(d == '1' || d == '2' || d == '3')){
         printf("Input invalid. Try again..\n");
         fflush(stdout);
@@ -38,23 +42,23 @@ char option_1(int max_value, int rand_num){
     char valid;
     printf("Enter a number(1 - %d): ", max_value);
     fflush(stdout);
-    valid = scanf("%d", guess_n) ? 'Y' : 'N';
+    valid = scanf("%d", &guess_n) ? 'Y' : 'N';
     if(valid == 'N'){
         char c = getchar();
         if(c == 'q' || c == 'Q')
             return STOP;
-        printf("Invalid input");
+        printf("Invalid input\n");
         return CONTINUE;
     }
     else{
         if(guess_n == rand_num){
-            printf("Congratulation! You have guessed %d right!", guess_n);
+            printf("Congratulation! You have guessed %d right!\n\n", guess_n);
             return STOP;
         }
         else if(guess_n > rand_num)
-            printf("Number too high. Try again.");
+            printf("Number too high. Try again.\n");
         else 
-            printf("Number too low. Try again.");
+            printf("Number too low. Try again.\n");
         
         return CONTINUE;
 
@@ -67,11 +71,39 @@ char option_1D(int rand_num){
 
 void Game(){
     srand(time(NULL));
-    int n = rand() % DEFAULT_VALUE + 1;
-    option_1D(n);
+    int n;
+    char end_program;
+    do{
+        displayMenu();
+        switch(inputValid()){
+            case '1' : {
+                n = rand() % DEFAULT_VALUE + 1;
+                char t;
+                do{
+                    t = option_1D(n);
+                }while(t == CONTINUE);
+                end_program = CONTINUE;
+                break;
+             }
+            case '2': {
+                break;
+            }
+            case '3' : {
+                printf("Thank you for playing!\n\n");
+                end_program = TERMINATE;
+                break;
+            }
+            default : break;
+        }
+        clear_bufferinput();
+    }while(end_program != TERMINATE);
+
 }
 
+
+
 int main(){
+    
     Game();
     return 0;
 }
